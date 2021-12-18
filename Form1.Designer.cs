56edd5f819e22751811c7f381ad93ea612d2356b
Alexey-Gaidai <71748686+Alexey-Gaidai@users.noreply.github.com>
2021-12-18 20:16:40 +0700

    patch0.1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,20 +29,289 @@ namespace Laba6
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.помощьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gauss_button = new System.Windows.Forms.Button();
+            this.quad_square_button = new System.Windows.Forms.Button();
+            this.prognka_button = new System.Windows.Forms.Button();
+            this.simple_itr_button = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 195);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(366, 272);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(430, 195);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(125, 272);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(384, 304);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 42);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "=";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(578, 195);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(210, 272);
+            this.dataGridView3.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(7, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Матрица А:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(425, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Вектор В:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(573, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Вектор Х:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(148, 473);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(407, 41);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "ГЕНЕРАЦИЯ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 484);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(34, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(48, 484);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(34, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(88, 485);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "х";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(20, 485);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "n =";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem,
+            this.очиститьToolStripMenuItem,
+            this.помощьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.очиститьToolStripMenuItem.Text = "Очистить";
+            // 
+            // помощьToolStripMenuItem
+            // 
+            this.помощьToolStripMenuItem.Name = "помощьToolStripMenuItem";
+            this.помощьToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.помощьToolStripMenuItem.Text = "Помощь";
+            // 
+            // gauss_button
+            // 
+            this.gauss_button.Location = new System.Drawing.Point(12, 27);
+            this.gauss_button.Name = "gauss_button";
+            this.gauss_button.Size = new System.Drawing.Size(206, 60);
+            this.gauss_button.TabIndex = 13;
+            this.gauss_button.Text = "Метод Гаусса";
+            this.gauss_button.UseVisualStyleBackColor = true;
+            this.gauss_button.Click += new System.EventHandler(this.gauss_button_Click);
+            // 
+            // quad_square_button
+            // 
+            this.quad_square_button.Location = new System.Drawing.Point(12, 100);
+            this.quad_square_button.Name = "quad_square_button";
+            this.quad_square_button.Size = new System.Drawing.Size(206, 60);
+            this.quad_square_button.TabIndex = 14;
+            this.quad_square_button.Text = "Метод квадратного корня";
+            this.quad_square_button.UseVisualStyleBackColor = true;
+            // 
+            // prognka_button
+            // 
+            this.prognka_button.Location = new System.Drawing.Point(224, 27);
+            this.prognka_button.Name = "prognka_button";
+            this.prognka_button.Size = new System.Drawing.Size(206, 60);
+            this.prognka_button.TabIndex = 15;
+            this.prognka_button.Text = "Метод прогонки";
+            this.prognka_button.UseVisualStyleBackColor = true;
+            // 
+            // simple_itr_button
+            // 
+            this.simple_itr_button.Location = new System.Drawing.Point(224, 100);
+            this.simple_itr_button.Name = "simple_itr_button";
+            this.simple_itr_button.Size = new System.Drawing.Size(206, 60);
+            this.simple_itr_button.TabIndex = 16;
+            this.simple_itr_button.Text = "Метод простой итерации";
+            this.simple_itr_button.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(436, 27);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(206, 60);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(436, 100);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(206, 60);
+            this.button7.TabIndex = 18;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 554);
+            this.ClientSize = new System.Drawing.Size(800, 526);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.simple_itr_button);
+            this.Controls.Add(this.prognka_button);
+            this.Controls.Add(this.quad_square_button);
+            this.Controls.Add(this.gauss_button);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem помощьToolStripMenuItem;
+        private System.Windows.Forms.Button gauss_button;
+        private System.Windows.Forms.Button quad_square_button;
+        private System.Windows.Forms.Button prognka_button;
+        private System.Windows.Forms.Button simple_itr_button;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,68 @@ namespace Laba6
         {
             InitializeComponent();
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox2.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+            dataGridView1.Rows.Clear();
+            dataGridView2.Rows.Clear();
+            dataGridView2.Columns.Clear();
+            Random rnd = new Random();
+            for (int i = 0; i < Convert.ToInt32(textBox1.Text); i++)
+            {
+                dataGridView1.Columns.Add("", "");
+            }
+            dataGridView1.Rows.Add(Convert.ToInt32(textBox1.Text)-1);
+            dataGridView2.Columns.Add("", "");
+            dataGridView2.Rows.Add(Convert.ToInt32(textBox1.Text)-1);
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = rnd.Next(0, 100);
+                }
+                dataGridView2.Rows[i].Cells[0].Value = rnd.Next(0, 100);
+            }
+        }
+
+        private void GaussMethod()
+        {
+            int[,] matrix = new int[Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox1.Text)+1];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1)-1; j++)
+                {
+                    matrix[i, j] = (int)dataGridView1.Rows[i].Cells[j].Value;
+                }
+                for (int j = matrix.GetLength(1) - 1; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = (int)dataGridView2.Rows[i].Cells[0].Value;
+                }
+            }
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                dataGridView3.Columns.Add("", "");
+            }
+            dataGridView3.Rows.Add(matrix.GetLength(0)-1);
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    dataGridView3.Rows[i].Cells[j].Value = matrix[i,j];
+                }
+            }
+        }
+
+        private void gauss_button_Click(object sender, EventArgs e)
+        {
+            GaussMethod();
+        }
     }
 }
